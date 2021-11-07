247d8ab588889aed1b532599a64d9c6371b66d2c
jadhavabhiee <92208809+jadhavabhiee@users.noreply.github.com>
2021-11-07 01:37:38 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const buttons = document.querySelectorAll('button');
+const display = document.querySelector('.display');
+
+buttons.forEach(function (button) {
+    button.addEventListener('click',calculate);
+});
+
+function calculate(event) {
+ const clickbuttonValue = event.target.value;    
+
+if (clickButtonValue === "=")
+{
+    if (display.value !== '')
+     {
+        display.value = eval(dispaly.value);
+   
+    }    
+
+} else
+
+     if (clickbuttonValue === 'C') 
+  {
+    display.value = '';
+    
+} else
+       {
+           display.value += clickedButtonVlaue;
+       }
+    }
